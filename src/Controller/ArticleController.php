9a2726dcf678463368790cdd29f24d932655b16e
Guillaume <guillaume-cornet07@hotmail.fr>
2018-05-18 11:57:05 +0200

    🏠 Add like random

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,10 +9,12 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -45,9 +47,11 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug)
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
     {
         // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearts');
 
         return new JsonResponse(['hearts' =>rand(5, 100)]);
     }
