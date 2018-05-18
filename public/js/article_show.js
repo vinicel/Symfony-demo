d8350d8c324f5653f56094cc49885000ccb95c60
Guillaume <guillaume-cornet07@hotmail.fr>
2018-05-18 11:40:39 +0200

    🏠 Add like random

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $('.js-like-article').on('click', function(e) {
         e.preventDefault();
+
         var $link = $(e.currentTarget);
         $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
-        $('.js-like-article-count').html('TEST');
+
+        $.ajax({
+            method: 'POST',
+            url: $link.attr('href')
+        }).done(function(data) {
+            $('.js-like-article-count').html(data.hearts);
+        })
     });
 });
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,6 +11,7 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
@@ -36,7 +37,18 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug)
+    {
+        // TODO - actually heart/unheart the article!
+
+        return new JsonResponse(['hearts' =>rand(5, 100)]);
     }
 }
